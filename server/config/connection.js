28ab7adef099ead795ecb846615ab3f68f571152
Robin <rrlangton@gmail.com>
2026-05-02 19:16:28 -0500

    updated mutations file name and reinstalled node on server

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/bookmarkd');
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/shelf-awareness');
 
 module.exports = mongoose.connection;
